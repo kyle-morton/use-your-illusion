4985ee71fcc1079c87e9361999702fede99eac9e
Kyle Morton <kylemortondevelopment@gmail.com>
2020-03-21 12:09:26 -0500

    Adding in correct class library type

---
+++ UseYourIllusion.Logic/Data/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseYourIllusion.Core.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
